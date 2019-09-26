b0903c5106b56c3877ae09a6a172f28c714428a5
Matt Kennedy <mjknj18@gmail.com>
2019-09-26 17:18:42 -0400

    Updated Financial Python Code.

--- Financial_Data_Analysis/Financial_Data_Analysis_Main.py
+++ Financial_Data_Analysis/Financial_Data_Analysis_Main.py
@@ -1,4 +1,5 @@
 import csv
+from statistics import mean
 
 with open('Budget_Data.csv') as input_file:
     budget_data = csv.reader(input_file, delimiter = ',')
@@ -16,9 +17,12 @@ with open('Budget_Data.csv') as input_file:
                     budget_column = count_2
 
                 budget_total = 0
+                change_amounts = []
             else:
                 if count_2 == budget_column:
                     budget_total = budget_total + int(column)
+
+                    change_amounts.append(int(column))
 
             count_2 = count_2 + 1
 
@@ -26,5 +30,22 @@ with open('Budget_Data.csv') as input_file:
         
     total_months = count_1 - 1
 
+    average_change = mean(change_amounts)
+
+    count_3 = 0
+
+    change_deltas = []
+
+    for item in change_amounts:
+        if count_3 == 0:
+            pass
+        else:
+            change_deltas.append(change_amounts[count_3] - change_amounts[count_3 - 1])
+
+        count_3 = count_3 + 1
+
+    average_change = round(mean(change_deltas), 2)
+
     print(total_months)
     print(budget_total)
+    print(average_change)
