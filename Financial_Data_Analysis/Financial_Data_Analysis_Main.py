6528676be3a983195983a8ddf7ed9d0d0ede7592
Matt Kennedy <mjknj18@gmail.com>
2019-09-26 18:23:40 -0400

    Updated Financial Python Code.

--- Financial_Data_Analysis/Financial_Data_Analysis_Main.py
+++ Financial_Data_Analysis/Financial_Data_Analysis_Main.py
@@ -18,11 +18,14 @@ with open('Budget_Data.csv') as input_file:
 
                 budget_total = 0
                 change_amounts = []
+                change_dates = []
             else:
                 if count_2 == budget_column:
                     budget_total = budget_total + int(column)
 
                     change_amounts.append(int(column))
+                else:
+                    change_dates.append(column)
 
             count_2 = count_2 + 1
 
@@ -35,6 +38,7 @@ with open('Budget_Data.csv') as input_file:
     count_3 = 0
 
     change_deltas = []
+    net_change = []
 
     for item in change_amounts:
         if count_3 == 0:
@@ -42,10 +46,28 @@ with open('Budget_Data.csv') as input_file:
         else:
             change_deltas.append(change_amounts[count_3] - change_amounts[count_3 - 1])
 
+            if change_amounts[count_3] > 0 and change_amounts[count_3 - 1] > 0:
+                net_change.append(abs(change_amounts[count_3]) - abs(change_amounts[count_3 - 1]))
+            elif change_amounts[count_3] > 0 and change_amounts[count_3 - 1] < 0:
+                net_change.append(abs(change_amounts[count_3]) + abs(change_amounts[count_3 - 1]))
+            elif change_amounts[count_3] < 0 and change_amounts[count_3 - 1] > 0:
+                net_change.append(-1 * (abs(change_amounts[count_3]) + abs(change_amounts[count_3 - 1])))
+            elif change_amounts[count_3] < 0 and change_amounts[count_3 - 1] < 0:
+                if change_amounts[count_3] > change_amounts[count_3 - 1]:
+                    net_change.append(-1 * (abs(change_amounts[count_3]) - abs(change_amounts[count_3 - 1])))
+                else:
+                    net_change.append(abs(change_amounts[count_3]) - abs(change_amounts[count_3 - 1]))
+            else:
+                net_change.append(0)
+
         count_3 = count_3 + 1
 
     average_change = round(mean(change_deltas), 2)
+    greatest_increase = max(net_change)
+    greatest_decrease = min(net_change)
 
     print(total_months)
     print(budget_total)
     print(average_change)
+    print(greatest_increase)
+    print(greatest_decrease)
